8d0f135fc1db4c18a0afdaf9d1149b6544170a62
Till Wehowski <github@webfan.de>
2022-06-20 16:32:26 +0200

    Update handsdown.php

--- handsdown.php
+++ handsdown.php
@@ -1,8 +1,14 @@
 <?php
 namespace Webfan\App\Frdlweb;
 
+use League\CommonMark\CommonMarkConverter;
+use Spyc;
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
+  
 
-$page = $_GET['page'];
+//$page = $_GET['page'];
+$page = ltrim($_SERVER['REQUEST_URI'], '/ ');
 if ($page == '') {
   $page = 'index';
 }
@@ -10,12 +16,18 @@ if ($page == '') {
 if (is_dir('pages/' . $page)) {
   $page .= '/index';
 }
- 
+
+//$result = find_and_parse_md_or_php_file('pages', $page, 'hmmm');
 $cms = new CMS();
 $result = $cms('pages', $page, 'hmmm');
 if ($result !== FALSE) {
   echo $result;
-}else{
+}
+else {
   header("HTTP/1.0 404 Not Found");
   echo $cms('pages', '404', '');
-} 
+}
+
+
+
+ 
